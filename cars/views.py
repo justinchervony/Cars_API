36451f9931daaa3e4d779d35dcaf77455f29238c
unknown <justinchervony@gmail.com>
2022-09-18 14:16:57 -0400

    Added serializer file

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Car
+
 @api_view(['GET'])
 def cars_list(request):
-    
+
     return Response('ok')
